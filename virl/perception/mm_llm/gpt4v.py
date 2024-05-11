3f1124926d80764415ae06cd086f1eb6a16305b2
jihanyang <jihanyang13@gmail.com>
2024-05-11 17:31:43 +0800

    update gpt4v

--- virl/perception/mm_llm/gpt4v.py
+++ virl/perception/mm_llm/gpt4v.py
@@ -11,6 +11,7 @@ class GPT4V(MultiModalLLMTemplate):
         super().__init__(cfg)
         self.client = OpenAI()
         self.api_key = os.getenv("OPENAI_API_KEY")
+        self.model_name = cfg.MODEL_NAME
 
     def predict(self, img, prompt):
         headers = {
@@ -23,7 +24,7 @@ class GPT4V(MultiModalLLMTemplate):
         content = self.add_image_to_content(img, content)
 
         payload = {
-            "model": "gpt-4-vision-preview",
+            "model": self.model_name,
             "messages": [
                 {
                     "role": "user",
@@ -35,7 +36,19 @@ class GPT4V(MultiModalLLMTemplate):
 
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
-        answer = response.json()['choices'][0]['message']['content']
+        if response.status_code != 200:
+            import ipdb; ipdb.set_trace()
+            type = response.json()['type']
+            if type in ['server_error']:
+                print(f"Retry due to {type}")
+                return self.predict(img, prompt)
+        
+        try:
+            answer = response.json()['choices'][0]['message']['content']
+        except:
+            print(response.json())
+            raise ValueError("Failed to get answer from GPT-4 Vision API")
+            
         return answer
 
     def add_image_to_content(self, image, content):
